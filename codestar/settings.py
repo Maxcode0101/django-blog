c383ca9a92ad20e1c9c48971f537386127294e82
Max Kaening <maximilian.kaening@gmail.com>
2024-12-02 14:59:34 +0000

    Changing database config and secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5*u*pkuy$o%&*avio8ixnoig=xah(*e5q%2@*1a9h)p3%9w9+o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
